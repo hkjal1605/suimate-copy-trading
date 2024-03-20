16bd986377a00563f3b9199a6ecc55a39f86fabe
Harsh <hkjal1605@gmail.com>
2024-03-20 16:38:46 +0530

    first commit

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,69 @@
+import '@/styles/global.css';
+
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { Nunito } from 'next/font/google'
+
+import { AppConfig } from '@/utils/AppConfig';
+
+export const metadata: Metadata = {
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+  ],
+};
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '600'],
+})
+
+
+export default function RootLayout(props: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  // Validate that the incoming `locale` parameter is valid
+  if (!AppConfig.locales.includes(props.params.locale)) notFound();
+
+  // Using internationalization in Client Components
+  const messages = useMessages();
+
+  return (
+    <html lang={props.params.locale} className={nunito.className}>
+      <body>
+        <NextIntlClientProvider
+          locale={props.params.locale}
+          messages={messages}
+        >
+          {props.children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
+
+// Enable edge runtime but you are required to disable the `migrate` function in `src/libs/DB.ts`
+// Unfortunately, this also means it will also disable the automatic migration of the database
+// And, you will have to manually migrate it with `drizzle-kit push`
+// export const runtime = 'edge';
