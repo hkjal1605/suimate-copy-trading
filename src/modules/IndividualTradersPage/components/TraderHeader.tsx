bed9824a5c971f8742484a7c2b6b4e0ef50798dd
Harsh <hkjal1605@gmail.com>
2024-03-24 12:07:53 +0530

    Started with individual traders page

---
+++ src/modules/IndividualTradersPage/components/TraderHeader.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import Avatar from 'boring-avatars';
+import getEllipsisTxt from '@/utils/getEllipsisText';
+import Image from 'next/image';
+
+interface IPropType {
+  address: string;
+}
+
+const TraderHeader = (props: IPropType) => {
+  const { address } = props;
+
+  return (
+    <div className="w-full flex px-5 py-3 justify-start items-center gap-4 border-b-[1px] border-black-400">
+      <Avatar
+        size={40}
+        name={address}
+        variant="beam"
+        colors={['#96ceb4', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+      />
+      <div className="flex flex-col items-start justify-center">
+        <div className="flex gap-1.5 justify-start items-center">
+          <p className="text-black-1000 text-base font-medium">
+            {getEllipsisTxt(address)}
+          </p>
+          <Image
+            src="/assets/images/copy.svg"
+            alt="Copy"
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/assets/images/star.svg"
+            alt="Star"
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <a href={`https://suivision.xyz/account/${address}`} target="_blank">
+            <Image
+              src="/assets/images/external.svg"
+              alt="View on Explorer"
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+          </a>
+        </div>
+        <div className="flex justify-start items-center gap-1">
+          <p className="text-xs text-black-700">Trader on</p>
+          <Image
+            src="/assets/images/platforms/bluefin.png"
+            alt="bluefin"
+            width={12}
+            height={12}
+          />
+          <p className="text-xs text-blue-200">Bluefin</p>
+        </div>
+      </div>
+      <a href="https://trade.bluefin.io/ETH-PERP" target="_blank" className='ml-auto'>
+        <div className="flex justify-center items-center gap-1.5 cursor-pointer">
+          <p className="text-base text-black-800">Trade on</p>
+          <Image
+            src="/assets/images/platforms/bluefin.png"
+            alt="bluefin"
+            width={16}
+            height={16}
+          />
+          <p className="text-base text-blue-200 font-semibold">Bluefin</p>
+        </div>
+      </a>
+      <div className=" h-10 w-px bg-black-400"></div>
+      <div className="flex justify-center items-center gap-1 cursor-pointer">
+        <Image
+          src="/assets/images/bell.svg"
+          alt="bell"
+          width={16}
+          height={16}
+        />
+        <p className="text-base text-black-800">Notify on New Trades</p>
+      </div>
+    </div>
+  );
+};
+
+export default TraderHeader;
--- src/modules/IndividualTradersPage/index.tsx
+++ src/modules/IndividualTradersPage/index.tsx
@@ -1,11 +1,16 @@
-"use client";
+'use client';
 
-import React from "react";
-import { usePathname } from "next/navigation";
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import TraderHeader from './components/TraderHeader';
 
 export default function IndividualTradersModule() {
   const pathname = usePathname();
-  const address = pathname.split("/").pop();
+  const address = pathname.split('/').pop() || '';
 
-  return <div className="text-black-900">Hello Trader Address: {address}</div>;
+  return (
+    <div className="w-full flex flex-col items-center justify-start">
+      <TraderHeader address={address} />
+    </div>
+  );
 }
