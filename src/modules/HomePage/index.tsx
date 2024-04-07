4cbbac16a567dcd7c5fc996bbbcb4eb24c074bfe
Harsh <hkjal1605@gmail.com>
2024-04-07 19:19:49 +0530

    Added spinner

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { OrderTradersBy } from './types/orderTradersBy';
 import fetchTopTraders from './utils/fetchTopTraders';
 import useTopTradersStore from '@/stores/useTopTradersStore';
+import Spinner from '@/components/Spinner';
 
 export default function HomePageModule() {
   const [topFilter, setTopFilter] = useState<OrderTradersBy>('netPnl');
@@ -20,11 +21,17 @@ export default function HomePageModule() {
     <div className="text-black-900 w-full">
       <div className="flex flex-col items-center justify-start px-5 w-full">
         <IntroFilter topFilter={topFilter} setTopFilter={setTopFilter} />
-        <div className="w-full grid grid-cols-3 gap-4">
-          {topTraders.map((trader) => (
-            <TraderCard trader={trader} />
-          ))}
-        </div>
+        {loaded ? (
+          <div className="w-full grid grid-cols-3 gap-4">
+            {topTraders.map((trader) => (
+              <TraderCard trader={trader} />
+            ))}
+          </div>
+        ) : (
+          <div className="w-full h-28 flex justify-center items-center">
+            <Spinner />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/modules/HomePage/utils/fetchTopTraders.ts
+++ src/modules/HomePage/utils/fetchTopTraders.ts
@@ -5,23 +5,26 @@ import { topTradersStore } from '@/stores/useTopTradersStore';
 
 const fetchTopTraders = async (orderBy: OrderTradersBy) => {
   try {
-    const queryTimestamp: number = 1680933000670
-    const limit = 15
+    topTradersStore.setState({
+      loaded: false
+    });
+
+    const queryTimestamp: number = 1680933000670;
+    const limit = 15;
 
     const response = await ApiService.getTopTraders({
       endTimestamp: queryTimestamp,
       limit,
       orderBy
-    })
+    });
 
     topTradersStore.setState({
       topTraders: response.data,
       loaded: true
-    })
-
+    });
   } catch (error) {
     logger.error(error);
   }
-}
+};
 
 export default fetchTopTraders;
